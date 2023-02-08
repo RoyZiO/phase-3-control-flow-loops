41b8bc080d5e3c6c8e234e203885375415bd7205
RoyZiO <nziokinroy@gmail.com>
2023-02-08 05:01:33 +0300

    Completed looping assignment

--- looping.rb
+++ looping.rb
@@ -1,5 +1,14 @@
 def happy_new_year
   # your code here
+  
+    count = 10
+  
+    until count == 0
+      puts count
+      count -= 1
+    end
+  
+    puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +26,28 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed = ""
+  index = str.length - 1
+  
+  while index >= 0
+    reversed += str[index]
+    index -= 1
+  end
+
+  return reversed
 end
